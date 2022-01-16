29871ba6a117661cf8f5abbb3366d252df4626c9
Aditya Prakash <prakashadityaap@gmail.com>
2022-01-16 17:02:56 +0530

    changed port in server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 require("dotenv").config()
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Developer', { useNewUrlParser: true , useUnifiedTopology: true })
     .then(() => console.log('MongoDB Connected...'))
