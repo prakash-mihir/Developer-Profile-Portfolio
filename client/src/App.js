f8067ae6d331e653b05e5c5d2eb4a5bd75321e1f
Aditya Prakash <prakashadityaap@gmail.com>
2022-01-17 15:33:47 +0530

    bugfix

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Landing from './components/Landing';
 import Footer from './components/Footer';
-import { HashRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Header from './components/devPage/Header';
 
