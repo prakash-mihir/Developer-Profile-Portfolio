f789e04ed1b5ce65ace029ca4d6c54bd270dc795
Aditya Prakash <prakashadityaap@gmail.com>
2022-01-17 09:57:56 +0530

    used github's api for repos

--- client/src/components/devPage/Repo.js
+++ client/src/components/devPage/Repo.js
@@ -3,37 +3,48 @@ import axios from 'axios';
 
 const Repo = () => {
     const [dev, setDev] = useState([])
+    const [dev2, setDev2] = useState([])
     
     // run only once
     useEffect(() => {
-        console.log("GETTING DEV");
         const github_id = window.location.pathname.slice(5);
         axios
             .get(`/api/devs/search/${github_id}`)
             .then((res) => {
                 if (res.data) {
                     setDev(res.data)
-                    console.log(dev);
                 }
             })
             .catch(console.error);
     }, []) 
+
+    useEffect(() => {
+        const github_id = window.location.pathname.slice(5);
+        axios
+            .get(`https://api.github.com/users/${github_id}/repos`)
+            .then((res) => {
+                if (res.data) {
+                    setDev2(res.data)
+                }
+                console.log(dev2);
+            })
+            .catch(console.error);
+    }, [])
     
     return(
         <div>
-            {dev[0] && dev[0].repos ? (
+            {dev2 ? (
                 // map all the repos into a list
                 <div className='repo-container'>
                     <div className="repo-title">Github repositories</div>
                     <hr></hr>
                     <ul className='repo-list'>
-                        {dev[0].repos.map((repo, index) => {
+                    {dev2.map((repo) => {
                             return (
-                                <div className="repo-item">
-                                    <li className='repo-name' key={index}>
-                                        {repo}
-                                    </li>
-                                    <hr></hr>
+                                <div className = "repo-item">
+                                <li className='repo-name'>
+                                    <a href={repo.html_url} target='_blank' rel='noopener noreferrer'>{repo.name}</a>
+                                </li>
                                 </div>
                             )
                         })}
