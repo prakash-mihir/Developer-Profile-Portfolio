1080dee559fade4c11f5d6ed5bc5f7a0a124a00a
Aditya Prakash <prakashadityaap@gmail.com>
2022-01-17 10:19:43 +0530

    potential fix for routing problem

--- routes/api.js
+++ routes/api.js
@@ -161,4 +161,8 @@ router.delete('/devs/:id', (req, res, next) => {
         });
 });
 
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 module.exports = router;
